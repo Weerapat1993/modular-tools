e1cb3eca683d653e142ba26791eb00cc7d90a0be
Top <weerapat.thawatchoketawee@gmail.com>
2018-01-23 21:21:02 +0700

    add Confirm

--- src/commands/clone.js
+++ src/commands/clone.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer')
+const { INQUIRER } = require('../config/command-list')
 const { findProject, MakeFile } = require('../utils')
  
 /**
@@ -7,15 +9,24 @@ const { findProject, MakeFile } = require('../utils')
  * @param {stirng} env 
  */
 const Clone = async (pwd, cmd, env) => {
-  findProject(pwd, (data, projectPath) => {
-    const file = new MakeFile(cmd, env, pwd)
-    const pwdSrc = `${pwd}/src`
-    const projectSrc = `${projectPath}/node_modules/${data.project_name}/src`
+  findProject(pwd, async (data, projectPath) => {
+    const confirm = await inquirer.prompt([
+      {
+        type: INQUIRER.confirm,
+        name: 'isConfirm',
+        message: 'Do you want to copy src folder?'
+      }
+    ])
+    if(confirm.isConfirm) {
+      const file = new MakeFile(cmd, env, pwd)
+      const pwdSrc = `${pwd}/src`
+      const projectSrc = `${projectPath}/node_modules/${data.project_name}/src`
 
-    file
-      .removeFolder(projectSrc)
-      .copyFolderTemplate(pwdSrc, projectSrc)
-      .status()
+      file
+        .removeFolder(projectSrc)
+        .copyFolderTemplate(pwdSrc, projectSrc)
+        .status()
+    }
   }) 
 }
 
