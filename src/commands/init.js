9adf1082ac3eba6f7f11019a280d717379c3bb33
Top <weerapat.thawatchoketawee@gmail.com>
2018-02-11 21:24:12 +0700

    Fix modular init

--- src/commands/init.js
+++ src/commands/init.js
@@ -2,22 +2,27 @@ const shell = require('shelljs')
 const { Log } = require('../utils')
 const log = new Log()
 
+// Git Project
+const GIT_PROJECT = 'git@bitbucket.org:dotography-code/scale-modular-kit.git'
+
 /**
  * Modular Clone
- * @param {stirng} pwd 
+ * @param {stirng} pwd s
  * @param {string} cmd 
  * @param {stirng} env 
  */
 const Init = async (pwd, cmd, env) => {
   if(env) {
-    shell.exec(`sudo ssh-add ~/.ssh/id_rsa`, { async: true }, () => {
-      shell.exec(`git clone git@bitbucket.org:dotography-code/scale-modular-kit.git ${env}`, { async: true }, (code, stdout, stderr) => {
+    shell.exec(`git clone ${GIT_PROJECT} ${env}`, { async: true }, (code, stdout, stderr) => {
+      if(!stderr) {
         log.success(`Modular Project '${env}' clone success.\n`)
         log.success(``)
         log.default(`cd ${env}`)
         log.default('rm -rf .git')
         log.default(`yarn`)
-      }) 
+      } else {
+        log.error(`Error: clone project failure.`)
+      }
     })
   } else {
     log.error('Please require `project_name`\n')
